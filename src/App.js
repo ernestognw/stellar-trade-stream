c3a2c5632f5e925658d036bedb439de49795f5c2
ernestognw <ernestognw@gmail.com>
2020-06-17 12:53:11 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { Asset } from "stellar-sdk";
+import { Horizon } from "./api";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend
+} from "recharts";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [data, setData] = useState([]);
+
+  const tradesHandler = trade => {
+    setData(data => [
+      ...data,
+      {
+        date: trade.ledger_close_time,
+        volume: Number(trade.base_amount),
+        price: trade.price.n / trade.price.d
+      }
+    ]);
+  };
+
+  useEffect(() => {
+    const disconnect = Horizon.trades()
+      /**
+       * @note Uncomment the following to have particular asset tracking.
+       */
+      // .forAssetPair(
+      //   new Asset(
+      //     "CENTUS",
+      //     "GAKMVPHBET4T7DPN32ODVSI4AA3YEZX2GHGNNSBGFNRQ6QEVKFO4MNDZ"
+      //   ),
+      //   Asset.native()
+      // )
+      .cursor("now")
+      .stream({
+        onmessage: tradesHandler
+      });
+
+    return () => disconnect();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <LineChart
+        width={500}
+        height={300}
+        data={data}
+        margin={{
+          top: 5,
+          right: 30,
+          left: 20,
+          bottom: 5
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="date" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Line
+          type="monotone"
+          dataKey="volume"
+          stroke="#8884d8"
+          activeDot={{ r: 8 }}
+        />
+        <Line
+          type="monotone"
+          dataKey="price"
+          stroke="#118cd8"
+          activeDot={{ r: 8 }}
+        />
+      </LineChart>
     </div>
   );
-}
+};
 
 export default App;
